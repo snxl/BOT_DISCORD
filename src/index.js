25a11965b9f49c26c5e8d8f9e445a89cfd77a85d
João Oliveira <joaoalan15@hotmail.com>
2021-08-03 21:54:16 -0300

    Upload files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('../config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	const { channel, content, author, createdTimestamp } = message;
+
+	if (author.bot || channel.name !== 'edu-bot') return;
+
+	const response = `sent a message on channel ${channel} at ${createdTimestamp}: ${content}`;
+
+	return message.reply(response);
+});
+
+client.on('messageReactionAdd', reaction => {
+	return reaction.message.channel.send(`${reaction.emoji.name} count: ${reaction.count}`);
+});
+
+client.login(token);
